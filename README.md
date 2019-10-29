6e14cc2d8ceaee4e8621b928ada111f8c70ce0d5
erika gonzalez <gonzalezerika75951@gmail.com>
2019-10-28 23:29:54 -0500

    yes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # friend_finder
+
+Friend Finder is an app that gives you 10 different questions that you can rate 1 (strongly disagree) to 5 (strongly agree), which will are random question. Once you have answered every single question it generates a new BFF for you with similar answers to those questions.
--- app/data/friend.js
+++ app/data/friend.js
@@ -5,18 +5,39 @@ var friendList = [
       name: 'Erika Gonzalez',
       profilePic: 'https://jrdunnphotography.smugmug.com/Sports/20182019-JHS-Girls-Soccer/i-ckWzcP8',
       scores: [
+        "5",
+        "4",
+        "3",
         "4",
         "5",
         "5",
-        "2",
         "4",
-        "5",
-        "4",
-        "4",
+        "3",
         "3",
         "4"
       ]
+    },
+    {
+      name: "Veronica Mendoza",
+      photo: "add pic",
+      scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+    },
+    {
+      name: "Henry Ford",
+      photo: "add pic",
+      scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+    },
+    {
+      name: "David Dobrik",
+      photo: "add pic",
+      scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+    },
+    {
+      name: "Margaret Millhow",
+      photo: "add pic",
+      scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
     }
-  ]
+  ];
+  
   
   module.exports = friendList;
